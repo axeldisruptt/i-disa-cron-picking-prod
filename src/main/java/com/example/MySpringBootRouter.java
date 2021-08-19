1d97c2fd744528cf9f901ffba9b91bda11320b92
Axel Ramiro <axel@alchimia.mx>
2021-08-19 18:17:43 -0500

    Producción

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -25,7 +25,7 @@ public class MySpringBootRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
     	
-    	String erpUri = "https://5298967-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=625&deploy=1";
+    	String erpUri = "https://5298967.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=6&deploy=1";
     	
     	onException(HttpOperationFailedException.class)
     		.handled(true)
--- src/main/java/com/example/OAuthSign.java
+++ src/main/java/com/example/OAuthSign.java
@@ -17,10 +17,10 @@ import oauth.signpost.signature.HmacSha256MessageSigner;
 
 public class OAuthSign {
 	public static String getAuthHeader(String uri) throws IOException {           
-	    String consumer_key = "594f75ba092ebe059027f6af0a6eb3b78cdb94419282f35c1de6b91803d708aa";
-	    String consumer_secret = "eba80a6a6f7374c5ba30220ca2fc7d4bf67db5120b8b327effadfe21c558de03";
-	    String access_token = "1baa836e6e43d0c06aea6eb30a7233ae1974969a367092574b95f5b2a715bc98";
-	    String access_secret= "e74aebe07b3827288b18776788d20b68ceae94777e4d26765884176fbabfa5b2";
+	    String consumer_key = "5c179380b0b2955dd1b379e9ce4a8f8f1fadad6d9bb2ae4bda878f468579eba2";
+	    String consumer_secret = "c95a5926e605b297c5fccdaa66fe34b48de30e39b209d56a29fe85b7a5a08ca6";
+	    String access_token = "b6bbb75dfc283fadd57ca34f22359b73939b9bd8089e5a905f6384c292945785";
+	    String access_secret= "6f86d81244f26659d3dffa38d131d0271b8617577258fb002bf36129e1d850b2";
 
 	    OAuthConsumer consumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
 	    consumer.setMessageSigner(new HmacSha256MessageSigner());
@@ -30,7 +30,7 @@ public class OAuthSign {
 	    
 	    try {
 	        HttpRequest signedReq = consumer.sign(httppost);
-	        String realm = "OAuth realm=\"5298967_SB1\",";
+	        String realm = "OAuth realm=\"5298967\",";
 	        return signedReq.getHeader("Authorization").toString().replace("OAuth", realm);
 	    } catch (OAuthMessageSignerException ex) {
 	        Logger.getLogger(HttpPost.class.getName()).log(Level.SEVERE, null, ex);
